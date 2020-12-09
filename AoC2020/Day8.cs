bacc8aa7c75a7697ccc722eb503684ca9cc4e84b
katilio <adanrodriguez@gmail.com>
2020-12-09 22:26:32 +0100

    Days 8 and 9, Parts 1 and 2

---
+++ AoC2020/Day8.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace AoC2020
+{
+    public class Day8
+    {
+        private string[] input = InputManager.StringArrayFromFile("day8.txt");
+
+        private enum CommandType
+        {
+            nop,
+            acc,
+            jmp
+        }
+
+        //Returns next index, writes to accumulator
+        private int ProcessCommand(CommandType cmd, int modifier, ref int accumulator)
+        {
+            int nextIndex = 1;
+            switch (cmd)
+            {
+                case (CommandType.acc):
+                    accumulator += modifier;
+                    break;
+                
+                case (CommandType.nop):
+                    break;
+                
+                case (CommandType.jmp):
+                    nextIndex = modifier;
+                    break;
+            }
+
+            return nextIndex;
+        }
+
+        public int Part1()
+        {
+            int accumulator = 0;
+            CommandType[] commands = new CommandType[input.Length];
+            int[] modifiers = new int[input.Length];
+            bool[] visited = new bool[input.Length];
+
+            //Populate from input
+            for (int i = 0; i < input.Length; i++)
+            {
+                var line = input[i];
+                
+                switch (line.Substring(0, 3))
+                    {
+                        case ("jmp"): commands[i] = CommandType.jmp;
+                            break;
+                        case("acc"): commands[i] = CommandType.acc;
+                            break;
+                        case ("nop"): commands[i] = CommandType.nop;
+                            break;
+                    }
+                modifiers[i] = Int32.Parse(line.Substring(4, line.Length-4));
+            }
+
+            for (int i = 0; i < commands.Length;)
+            {
+                if (visited[i] == true)
+                {
+                    Console.WriteLine("Accumulator: " + accumulator);
+                    break;
+                }
+                visited[i] = true;
+                i += ProcessCommand(commands[i], modifiers[i], ref accumulator);
+            }
+
+            return accumulator;
+        }
+        
+        public int Part2()
+        {
+            int accumulator = 0;
+            CommandType[] originalCommands = new CommandType[input.Length];
+            var commands = originalCommands;
+            int[] modifiers = new int[input.Length];
+            bool[] visited = new bool[input.Length];
+            
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                accumulator = 0;
+                visited = new bool[input.Length];
+                for (int k = 0; k < input.Length; k++)
+                {
+                    var line = input[k];
+                
+                    switch (line.Substring(0, 3))
+                    {
+                        case ("jmp"): commands[k] = CommandType.jmp;
+                            break;
+                        case("acc"): commands[k] = CommandType.acc;
+                            break;
+                        case ("nop"): commands[k] = CommandType.nop;
+                            break;
+                    }
+                }
+                if (commands[i] == CommandType.jmp || commands[i] == CommandType.nop)
+                {
+                    commands[i] = (commands[i] == CommandType.jmp) ? CommandType.nop : CommandType.jmp;
+                    for (int j = 0; j < commands.Length;)
+                    {
+                        if (j == (commands.Length-1))
+                        {
+                            j += ProcessCommand(commands[j], modifiers[j], ref accumulator);
+                            Console.WriteLine("Part 2: accumulator =" + accumulator);
+                            break;
+                        }
+                        else if (visited[j] == true)
+                        {
+                            break;
+                        }
+                        visited[j] = true;
+                        j += ProcessCommand(commands[j], modifiers[j], ref accumulator);
+                    }
+                }
+            }
+
+
+            return accumulator;
+        }
+        
+    }
+}
---
+++ AoC2020/Day9.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace AoC2020
+{
+    public class Day9
+    {
+        private string[] input = InputManager.StringArrayFromFile("day9.txt");
+
+        private bool IsValidNumber(long[] numbers, int index, int preambleLen)
+        {
+            int startIndex = index - preambleLen;
+            for (int i = startIndex; i < index; i++)
+            {
+                for (int j = startIndex; j < index; j++)
+                {
+                    if (numbers[j] + numbers[i] == numbers[index] && i != j)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private long[] FindContiguousSet(long[] numbers, long sum)
+        {
+            var filtered = numbers.TakeWhile(x => x < sum).ToArray();
+            
+            for (int j = 1; j < filtered.Count()-1; j++)
+            {
+                for (int i = filtered.Count()-1; i > j; i--)
+                {
+                    var summed = new long[i-j];
+                    Array.Copy(filtered, j, summed, 0, i-j);
+                    if (summed.Sum(x => x) == sum)
+                    {
+                        return summed;
+                    }
+                }
+            }
+
+            Console.WriteLine("Couldn't find a set.");
+            return new long[0];
+        }
+        public void Part1()
+        {
+            long invalidNumber = 0;
+            long[] numbers = new long[input.Length];
+            for (int i = 0; i < input.Length; i++)
+            {
+                numbers[i] = long.Parse(input[i]);
+            }
+
+            for (int i = 25; i < numbers.Length; i++)
+            {
+                if (!IsValidNumber(numbers, i, 25))
+                {
+                    Console.WriteLine("Day 9, part 1: " + numbers[i]);
+                    invalidNumber = numbers[i];
+                }
+            }
+            var set = FindContiguousSet(numbers, invalidNumber);
+            Console.WriteLine("Day 9, part 2 : " + (set.Min() + set.Max()));
+
+        }
+    }
+}
